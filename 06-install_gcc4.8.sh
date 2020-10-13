b6b02a5af5a85505e304a73f97cb1a319696c623
rodrigocruz13 <rodrigocruzayala@gmail.com>
2020-10-13 11:28:56 -0500

    Fixed alternatives options

--- 06-install_gcc4.8.sh
+++ 06-install_gcc4.8.sh
@@ -22,7 +22,7 @@ sudo apt-get install python-software-properties
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt-get update
 sudo apt-get install gcc-4.8
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 0
 
 printf "\n\t${COLOR}Cleaning${NC}\n"
 sudo apt autoremove -y
